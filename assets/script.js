3501ea76b14f5435b3b1ec97c3c990c42f9e5a7a
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-07-22 21:17:26 -0400

    Quiz functions

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,8 @@ var button1 = document.getElementById("option1");
 var button2 = document.getElementById("option2");
 var button3 = document.getElementById("option3");
 var button4 = document.getElementById("option4");
+var highscoreInputName = document.getElementById("name");
+
 
 const question_result = document.querySelector("#correctness");
 const final_results = document.querySelector("#end-result");
@@ -60,3 +62,56 @@ const questions = [
         correctAnswer: "1"
     }
 ]
+
+var totalQuestions = questions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 91;
+var timerInterval;
+var score = 0;
+var correctSelection;
+
+document.querySelector("#start-button").addEventListener("click", startQuiz);
+
+//Selection of question and loop to iterate through array of questions and their options
+function showQuestion(){
+    let question = questions[currentQuestionIndex];
+    let options = question.options;
+
+    let questionEl = document.querySelector("#question-text");
+    questionEl.textContent = question.questionText;
+
+    for (i=0; i < options.length; i++){
+        let option = options[i];
+        let optionButton = document.querySelector("#option" + i);
+        optionButton.textContent = option;
+    }
+}
+
+function displayFinalScore(){
+    quiz_start.style.display = "none";
+    end_result.style.display = "flex";
+    clearInterval(timerInterval);
+    highscoreInputName.value = "";
+    final_score.textContent = "You got " + score + " of a total " + totalQuestions + " questions correct!";
+}
+
+function startQuiz(){
+    //Clear cards for the questions to be displayed
+    hideCards();
+    questionCard.removeAttribute("hidden");
+
+    //Setting first question to display for the start of the quiz
+    currentQuestionIndex = 0;
+    showQuestion();
+
+    timerInterval = setInterval(function(){
+        timeLeft--;
+        quizTimer.textContent = "Remaining Time: " + timeLeft;
+
+        if(timeLeft === 0) {
+            clearInterval(timerInterval);
+            displayFinalScore();
+        }
+    }, 1000);
+    quiz_start.style.display = "block";
+}
