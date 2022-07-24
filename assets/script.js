2e113adad9b4527d41c0902e1b04fb0b98d95625
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-07-24 17:49:49 -0400

    Extra

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,42 @@
 //Variable storage
+
+const High_Scores_Key = "highScores";
+
+const highScores = JSON.parse(localStorage.getItem(High_Scores_Key)) || [];
+
+const questions = [
+    {
+        //Question 1
+        questionText: "The condition in an if / else statement is enclosed within ____.",
+        options: ["1. Curly Brackets", "2. Quotes", "3. Parentheses", "4. Square Brackets"],
+        correctAnswer: 1
+    },
+    {
+        //Question 2
+        questionText: "The first index of an array is ____.",
+        options: ["1. 5", "2. 1", "3. 0", "User chooses"],
+        correctAnswer: 2
+    },
+    {
+        //Question 3
+        questionText: "String values must be enclosed using ____ when they are assigned to variables.",
+        options: ["1. Parentheses", "2. Periods", "3. Curly Brackets", "4. Quotation Marks"],
+        correctAnswer: 2
+    },
+    {
+        //Question 4
+        questionText: "How can you quickly debug by printing out values during development?",
+        options: ["1. Terminal/bash", "2. Terminal/zsh", "3. System.print(TELL ME)", "4. Console.log"],
+        correctAnswer: 3
+    },
+    {
+        //Question 5
+        questionText: "If you wanted to check if two variables are equal to each other in an if/else statement you would use ____.",
+        options: ["1. ==", "2. 'equals'", "3. ===", "4. ="],
+        correctAnswer: 0
+    }
+];
+
 const startScreenEl = document.getElementById("start-screen");
 const startButtonEl = document.getElementById("start-button");
 
@@ -93,6 +131,48 @@ const endQuiz = () = => {
     scoreSpanEl.textContent = score;
     endScreenEl.hidden = false;
 };
+
+const displayHighScores = () => {
+        if (!highScoreListEl) {
+            return;
+        }
+        highScoreListEl.innerHTML = "";
+        highScores.forEach((highScore) => {
+        const liEl = document.createElement("li");
+        liEl.textContent = ${highScore.initials} - ${highScore.score};
+        highScoreListEl?.appendChild(liEl);
+    });
+};
+
+startButtonEl?.addEventListener("click", startQuiz);
+
+scoreFormEl?.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(scoreFormEl);
+    const initials = formData.get("initials");
+    if (!initials) {
+        alert("Enter initials");
+        return false;
+    }
+    highScores.push({
+        initials,
+        score,
+    });
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(10);
+    localStorage.setItem(High_Scores_Key, JSON.stringify(highSCores));
+    startScreenEl.hidden = true;
+    quizScreenEl.hidden = true;
+    endScreenEl.hidden = true;
+    displayHighScores();
+    highScoreScreenEl.hidden = false;
+});
+
+clearHighScoresButtonEl?.addEventListener("click", () => {
+    localStorage.removeItem(High_Scores_Key);
+    location.reload();
+});
+ 
 
 //Hiding cards
 function hideCards(){
